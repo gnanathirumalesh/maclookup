d06020ab5446387597fd1296dad64444df27469d
gnanathirumalesh <149413353+gnanathirumalesh@users.noreply.github.com>
2023-11-12 22:16:47 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const cors = require('cors')
+const port = 3000;
+
+// Middleware to parse JSON data
+app.use(express.json());
+app.use(cors())
+app.get('/mac/:macAddress', async (req, res) => {
+  try {
+    const macAddress = req.params.macAddress;
+    const apiUrl = `https://api.maclookup.app/v2/macs/${macAddress}`;
+
+    // Make a GET request to the MAC address lookup API
+    const response = await axios.get(apiUrl);
+
+    // Extract relevant data from the response
+    const {
+      success,
+      found,
+      macPrefix,
+      company,
+      address,
+      country,
+      blockStart,
+      blockEnd,
+      blockSize,
+      blockType,
+      updated,
+      isRand,
+      isPrivate,
+    } = response.data;
+
+    // Send the formatted JSON response
+    res.json({
+      success,
+      found,
+      macPrefix,
+      company,
+      address,
+      country,
+      blockStart,
+      blockEnd,
+      blockSize,
+      blockType,
+      updated,
+      isRand,
+      isPrivate,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
