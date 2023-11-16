e29f6a261cf63d0607756f0d6b89f26d5743c00d
gnanathirumalesh <149413353+gnanathirumalesh@users.noreply.github.com>
2023-11-16 19:04:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const cors = require('cors')
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 // Middleware to parse JSON data
 app.use(express.json());
